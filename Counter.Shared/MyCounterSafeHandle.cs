f7462d5fb5b7438893af29e6aab177e6f1643a66
Wes Hodges <wes.hodges@synaptivemedical.com>
2023-01-31 17:15:20 -0500

    Missed a file

---
+++ Counter.Shared/MyCounterSafeHandle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Win32.SafeHandles;
+
+namespace Counter
+{
+    internal class MyCounterSafeHandle : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        public MyCounterSafeHandle() : base(true) { }
+
+        public IntPtr Ptr => handle;
+
+        protected override bool ReleaseHandle()
+        {
+            CounterWrapper.DeleteCounter(this);
+            return true;
+        }
+    }
+}
