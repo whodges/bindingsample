c20654bb82af94469a0fe478622f8d08325fa8ea
Wes Hodges <wes.hodges@synaptivemedical.com>
2023-01-31 16:41:24 -0500

    Moved to .NET 7.0 (required a fresh MAUI project)

---
+++ BasicMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BasicMauiApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Counter;
+using System.Diagnostics.Metrics;
+
+namespace BasicMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        MyCounter _externalCounter;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _externalCounter = new MyCounter();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            var count = _externalCounter.GetCount();
+            _externalCounter.SetCount(count + 1);
+            count = _externalCounter.GetCount();
+
+            CounterBtn.Text = $"Clicked {count} times";
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time (" + _externalCounter.GetDllName() + ")";
+            else
+                CounterBtn.Text = $"Clicked {count} times (" + _externalCounter.GetDllName() + ")";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
